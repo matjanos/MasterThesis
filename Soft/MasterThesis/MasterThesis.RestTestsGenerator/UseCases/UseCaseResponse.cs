ea96f58f5ac3e2b015972218ec8e7b490e9df17b
Kuba Matjanowski <k.matjanowski@gmail.com>
2016-08-10 21:40:47 +0200

    This part is not responsible for asserting (just for telling the type of assertion)

--- Soft/MasterThesis/MasterThesis.RestTestsGenerator/UseCases/UseCaseResponse.cs
+++ Soft/MasterThesis/MasterThesis.RestTestsGenerator/UseCases/UseCaseResponse.cs
@@ -14,14 +14,5 @@ namespace MasterThesis.RestTestsGenerator.UseCases
 
         //TODO: maybe stream?
         public string Body { get; private set; }
-
-        public bool AssertEquality(UseCaseResponse useCaseResponse)
-        {
-            if (Code != useCaseResponse.Code)
-                return false;
-
-            //TODO: if all required fields are present and equal and other fields are equal if exist (or strategies)
-            return true;
-        }
     }
 }
